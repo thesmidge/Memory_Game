763464d36f20c41ad77e23e2525b10eb8081eed3
thesmidge <134163553+thesmidge@users.noreply.github.com>
2023-08-15 20:28:19 -0600

    Update app.js
    Add flipCard function

--- app.js
+++ app.js
@@ -51,19 +51,35 @@ document.addEventListener('DOMContentLoaded', () =>{
     },
   ]
   const grid = document.querySelector('.grid');
+  let cardsChosen = [];
+  let cardsChosenId = [];
 
+  //create your board
   function createBoard(){
     for(let i = 0; i < cardArray.length; i++){
       let card = document.createElement('img');
       card.setAttribute('src', 'images/blank.png');
-      card.setAttribute('data-id', i)
+      card.setAttribute('data-id', i);
       // card.addEventListener('click', flipcard);
-      grid.appendChild(card)
+      grid.appendChild(card);
     }
   }
-  createBoard()
 
-})
+  //check for matches
+
+
+  //flip your card
+  function flipCard() {
+    let cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute('src', cardArray[cardId].img);
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500);
+  }
+  createBoard();
+
+}
 
 
 

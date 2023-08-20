2bb4a96bc4daa9722ebe42f4e95a1d4b96754660
thesmidge <134163553+thesmidge@users.noreply.github.com>
2023-08-20 16:38:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
 
   const cardArray = [
     {
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     {
       name: 'dragon6',
       img: 'images/dragon6.jpeg'
-    },
+    }
   ]
   
   cardArray.sort(() => 0.5 - Math.random());
@@ -60,34 +60,34 @@ document.addEventListener('DOMContentLoaded', () =>{
   let cardsWon = [];
 
   //create your board
-  function createBoard(){
-    for(let i = 0; i < cardArray.length; i++){
+  function createBoard() {
+    for(let i = 0; i < cardArray.length; i++) {
       let card = document.createElement('img');
       card.setAttribute('src', 'images/blank.png');
       card.setAttribute('data-id', i);
-      card.addEventListener('click', flipcard);
+      card.addEventListener('click', flipCard);
       grid.appendChild(card);
     }
   }
 
   //check for matches
   function checkForMatch() {
-    var cards = document.querySelectorAll('img')
-    const optionOneId = cardsChosenId[0]
-    const optionTwoId = cardsChosenId[1]
+    let cards = document.querySelectorAll('img');
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
     if (cardsChosen[0] === cardsChosen[1]) {
-      alert('You found a match')
-      cards[optionOneId].setAttribute('src', 'images/white.png')
-      cards[optionTwoId].setAttribute('src', 'images/white.png')
-      cardsWon.push(cardsChosen)
+      alert('You found a match');
+      cards[optionOneId].setAttribute('src', 'images/white.png');
+      cards[optionTwoId].setAttribute('src', 'images/white.png');
+      cardsWon.push(cardsChosen);
     } else {
-      cards[optionOneId].setAttribute('src', 'images/blank.png')
-      cards[optionTwoId].setAttribute('src', 'images/blank.png')
-      alert('Sorry try again')
+      cards[optionOneId].setAttribute('src', 'images/blank.png');
+      cards[optionTwoId].setAttribute('src', 'images/blank.png');
+      alert('Sorry try again');
     }
-    cardsChosen = []
-    cardsChosenId =[]
-    resultDisplay.textContent = cardsWon.length
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = cardsWon.length;
     if (cardsWon.length === cardArray.length/2) {
       resultDisplay.textContent = 'Congratulations! You found them all!';
     }
